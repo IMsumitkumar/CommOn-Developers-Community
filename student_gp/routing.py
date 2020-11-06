713ebac06daf901a385a22b6d89f29b93a8b4bb7
IMSumitKumar <sk20506942@gmail.com>
2020-11-06 09:14:00 -0800

    updated channels version

--- student_gp/routing.py
+++ student_gp/routing.py
@@ -4,6 +4,6 @@ from . import consumers as a
 from user_group import consumers as b
 
 websocket_urlpatterns = [
-    re_path(r'ws/student_gp/(?P<room_name>\w+)/$', a.ChatConsumer.as_asgi()),
-    re_path(r'ws/ug_api/(?P<room_name>[^/]+)/$', b.ChatConsumer.as_asgi()),
+    re_path(r'ws/student_gp/(?P<room_name>\w+)/$', a.ChatConsumer),
+    re_path(r'ws/ug_api/(?P<room_name>[^/]+)/$', b.ChatConsumer),
 ]
